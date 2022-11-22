554e8aadee2d4bedb70bbbf17231fae35df11c84
Pietro Telino <pietrotelino@hotmail.com>
2022-11-22 08:45:16 -0300

    Aula 06 Single File Vue Components

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="about">
     <h1>Event List Page</h1>
-    <router-link :to="{ name: 'event-show', params: { id: 1 } }">
-      Show event #1
-    </router-link>
+    <EventCard />
   </div>
 </template>
+
+<script>
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  components: {
+    EventCard,
+  },
+};
+</script>
