5ed85ac3ab5b198ec11c216799c0dd5a6f313d78
Pietro Telino <pietrotelino@hotmail.com>
2022-12-02 08:13:39 -0300

    aula 08 do curso Next-Level Vue 2

--- real-world-vue/src/router/index.js
+++ real-world-vue/src/router/index.js
@@ -6,6 +6,8 @@ import EventShow from "../views/EventShow.vue";
 import User from "../views/User.vue";
 import NProgress from "nprogress";
 import store from "@/store";
+import NotFound from "../views/NotFound.vue";
+import NetworkIssue from "../views/NetworkIssue.vue";
 
 Vue.use(VueRouter);
 
@@ -16,16 +18,26 @@ const routes = [
     component: EventShow,
     props: true,
     beforeEnter(routeTo, routeFrom, next) {
-      store.dispatch("event/fetchEvent", routeTo.params.id).then((event) => {
-        routeTo.params.event = event;
-        next();
-      });
+      store
+        .dispatch("event/fetchEvent", routeTo.params.id)
+        .then((event) => {
+          routeTo.params.event = event;
+          next();
+        })
+        .catch((error) => {
+          if (error.response && error.response.status == 404) {
+            next({ name: "404", params: { resource: "event" } });
+          } else {
+            next({ name: "network-issue" });
+          }
+        });
     },
   },
   {
     path: "/",
     name: "event-list",
     component: EventList,
+    props: true,
   },
   {
     path: "/event/create",
@@ -37,6 +49,21 @@ const routes = [
     name: "user",
     component: User,
     props: true,
+  },
+  {
+    path: "/404",
+    name: "404",
+    component: NotFound,
+    props: true,
+  },
+  {
+    path: "/network-issue",
+    name: "network-issue",
+    component: NetworkIssue,
+  },
+  {
+    path: "*",
+    redirect: { name: "404", params: { resource: "page" } },
   },
 ];
 
--- real-world-vue/src/store/modules/event.js
+++ real-world-vue/src/store/modules/event.js
@@ -6,6 +6,7 @@ export const state = {
   events: [],
   eventsTotal: 0,
   event: {},
+  perPage: 3,
 };
 export const getters = {
   getEventById: (state) => (id) => {
@@ -46,8 +47,8 @@ export const actions = {
         throw error;
       });
   },
-  fetchEvents({ commit, dispatch }, { perPage, page }) {
-    EventService.getEvents(perPage, page)
+  fetchEvents({ commit, dispatch, state }, { page }) {
+    return EventService.getEvents(state.perPage, page)
       .then((response) => {
         console.log(`Total events are ${response.headers["x-total-count"]}`);
         commit("SET_EVENTS_TOTAL", parseInt(response.headers["x-total-count"]));
@@ -61,24 +62,21 @@ export const actions = {
         dispatch("notification/add", notification, { root: true });
       });
   },
-  fetchEvent({ commit, getters, dispatch }, id) {
+  fetchEvent({ commit, getters, state }, id) {
+    if (id == state.event.id) {
+      return state.event;
+    }
+
     var event = getters.getEventById(id);
+
     if (event) {
       commit("SET_EVENT", event);
       return event;
     } else {
-      return EventService.getEvent(id)
-        .then((response) => {
-          commit("SET_EVENT", response.data);
-          return response.data;
-        })
-        .catch((error) => {
-          const notification = {
-            type: "error",
-            message: `There was a problem fetching event: ${error.message}`,
-          };
-          dispatch("notification/add", notification, { root: true });
-        });
+      return EventService.getEvent(id).then((response) => {
+        commit("SET_EVENT", response.data);
+        return response.data;
+      });
     }
   },
 };
